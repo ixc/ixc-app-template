2a7bf1aa5a7558d171f820be19fd1f5b025258dc
Tai Lee <real.human@mrmachine.net>
2016-04-04 15:18:51 +1000

    Update setup.py
    * Convert README.md to reStructuredText long description.
    * Add missing package metadata.
    * Require `psycopg2`.
    * Don't assume license will be MIT.

--- setup.py
+++ setup.py
@@ -1,9 +1,32 @@
+from __future__ import print_function
+
 import setuptools
+import sys
+
+# Convert README.md to reStructuredText.
+if {'bdist_wheel', 'sdist'}.intersection(sys.argv):
+    try:
+        import pypandoc
+    except ImportError:
+        print('WARNING: You should install `pypandoc` to convert `README.md` '
+              'to reStructuredText to use as long description.',
+              file=sys.stderr)
+    else:
+        print('Converting `README.md` to reStructuredText to use as long '
+              'description.')
+        long_description = pypandoc.convert('README.md', 'rst')
 
 setuptools.setup(
     name='{{ app_name }}',
     use_scm_version={'version_scheme': 'post-release'},
+    author='Interaction Consortium',
+    author_email='studio@interaction.net.au',
+    url='https://github.com/ixc/{{ app_name }}',
+    description='',
+    long_description=locals().get('long_description', ''),
+    license='',
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         'coverage',
         'django-dynamic-fixture',
@@ -11,11 +34,11 @@ setuptools.setup(
         'django-webtest',
         'mkdocs',
         'nose-progressive',
+        'psycopg2',
         'WebTest',
     ],
     extras_require={
         'dev': ['ipdb', 'ipython'],
-        'postgres': ['psycopg2'],
     },
     setup_requires=['setuptools_scm'],
 )
